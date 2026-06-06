dd34feb8ceeeefe0d5e5214f202f679909b7707a
Gabriel Resende Spirlandelli <grspirlandelli@gmail.com>
2026-06-06 10:24:17 -0300

    feat: Adicioando documentação a relatórios

--- src/routes/relatorios.js
+++ src/routes/relatorios.js
@@ -3,9 +3,20 @@ import { getRelatorioVendas } from '../services/relatorioService.js';
 
 const router = Router();
 
+/**
+ * @swagger
+ * /relatorios/vendas:
+ *   get:
+ *     summary: Relatório consolidado de vendas
+ *     parameters:
+ *       - $ref: '#/components/parameters/filtrarGrupoParam'
+ *     responses:
+ *       200:
+ *         description: Relatório
+ */
 router.get('/vendas', async (req, res) => {
   try {
-    const relatorio = await getRelatorioVendas();
+    const relatorio = await getRelatorioVendas(req.query);
     res.json(relatorio);
   } catch (error) {
     res.status(500).json({ error: error.message });
